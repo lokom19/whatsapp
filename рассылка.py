f28c816bd3f4e87f16dd3aac0d34f7de9c847244
lokom19 <87856931+lokom19@users.noreply.github.com>
2022-06-27 17:30:38 +0300

    Add files via upload

---
+++ рассылка.py
@@ -0,0 +1,14 @@
+import pywhatkit
+#поиск видео в ютубе
+#pywhatkit.playonyt('Сердце пацана')
+
+#поиск гугл
+#pywhatkit.search('шлагбаума')
+
+#pywhatkit.sendMail('nasty.shpakova@gmail.com', '21021975Nn', 'nasty.shpakova@gmail.com', 'Hey babe')
+
+
+pywhatkit.sendwhatmsg('+79296797683', 'Привет', 14, 20)
+#pywhatkit.sendMail('lokom19@mail.ru', '31072002makey',  'nasty.shpakova@gmail.com' ,  'C:\my_foto.jfif')
+
+# pywhatkit.sendMail ( 'lokom19@mail.ru', '31072002Lokom19',  'nasty.shpakova@gmail.com' ,  'Hello N')
